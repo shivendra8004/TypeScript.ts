71c2307a789fa43b45419132c9a0eee59460f097
SHIVENDRA8004 <jat13632@gmail.com>
2023-04-17 19:20:55 +0530

    Reseted code to teacher code

--- src/components/Greet.tsx
+++ src/components/Greet.tsx
@@ -1,7 +1,7 @@
-// type GreetProps = {
-//   name: string;
-// };
-export const Greet = (props: { name: string }) => {
+type GreetProps = {
+  name: string;
+};
+export const Greet = (props: GreetProps) => {
   return (
     <div>
       <h2>Welcome {props.name}! You have 69 unread messages</h2>
