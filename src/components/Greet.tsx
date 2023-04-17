e27312682a2f059e7340376d233ab2a7c4e19b6b
SHIVENDRA8004 <jat13632@gmail.com>
2023-04-17 19:20:32 +0530

    This is the another method to set the type of props

--- src/components/Greet.tsx
+++ src/components/Greet.tsx
@@ -1,7 +1,7 @@
-type GreetProps = {
-  name: string;
-};
-export const Greet = (props: GreetProps) => {
+// type GreetProps = {
+//   name: string;
+// };
+export const Greet = (props: { name: string }) => {
   return (
     <div>
       <h2>Welcome {props.name}! You have 69 unread messages</h2>
