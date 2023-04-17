c754822f9fe3b36538f65dad201b1c49d4f4d46f
SHIVENDRA8004 <jat13632@gmail.com>
2023-04-18 00:51:50 +0530

    Removed previous component

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,10 @@
 import React from "react";
 import "./App.css";
-import Greet from "./components/Greet";
-import Person from "./components/Person";
-import PersonList from "./components/PersonList";
 import Status from "./components/Status";
 
 function App() {
-  const personName = {
-    first: "Shivendra",
-    last: "Jat",
-  };
-  const nameList = [
-    {
-      first: "Jyoti",
-      last: "Kumari",
-    },
-    {
-      first: "Shivendra",
-      last: "Jat",
-    },
-    {
-      first: "Anandi",
-      last: "Tripathi",
-    },
-  ];
   return (
     <div className="App">
-      <Greet name="Shivendra" messageCount={20} isLoggedIn={true} />
-      <Person personName={personName} />
-      <PersonList nameList={nameList} />
       <Status status="error" />
     </div>
   );
