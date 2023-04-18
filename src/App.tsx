61f97676e149cc41b55fe24722228f03003348b4
SHIVENDRA8004 <jat13632@gmail.com>
2023-04-19 00:31:18 +0530

    Lect 7 finished: Learned about ts styles props

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import Container from "./components/Container";
 function App() {
   return (
     <div className="App">
-      <Container styles={{ border: "1px solid black", padding: "1rem" }} />
+      <Container styles={{ border: "1px solid black", padding: "1rem", background: "green" }} />
     </div>
   );
 }
