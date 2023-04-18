779e6d01a95434b5cdf80fccb6b7812138a8bf73
SHIVENDRA8004 <jat13632@gmail.com>
2023-04-18 08:06:35 +0530

    Successfully handled onChange value using event props

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import "./App.css";
 import Button from "./components/Button";
+import Input from "./components/Input";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
           console.log("Button Clicked", e, id);
         }}
       />
+      <Input inputValue="" onChangeValue={(event) => console.log(event.target.value)} />
     </div>
   );
 }
