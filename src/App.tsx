b94eaac185f65e418a3b7a6ef54482cb3fe5ada2
SHIVENDRA8004 <jat13632@gmail.com>
2023-04-18 08:08:03 +0530

    Lect6 finished: Learned about various event props or we can say we can say how to define type of an event prop

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,9 @@
 import "./App.css";
-import Button from "./components/Button";
 import Input from "./components/Input";
 
 function App() {
   return (
     <div className="App">
-      <Button
-        handleClick={(e, id) => {
-          console.log("Button Clicked", e, id);
-        }}
-      />
       <Input inputValue="" onChangeValue={(event) => console.log(event.target.value)} />
     </div>
   );
